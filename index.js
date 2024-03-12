b6dd1e70daa55ee3577f3efe946249e9f448188b
maryjanemugweru <maryjanemugweru@gmail.com>
2024-03-12 12:55:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const studentRoute =require("./routes/studentRoute");
+
+app.use('/api/student', studentRoute);
+
+app.listen(process.env.port || 4000, function() {
+    console.log("Now listening for requests on: http://localhost:4000");
+});
